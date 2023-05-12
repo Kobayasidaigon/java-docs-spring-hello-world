e53bdf7d80e5a3035864a8af4bf8827b1fd873c5
Kobayasidaigon <49702049+Kobayasidaigon@users.noreply.github.com>
2023-05-13 00:30:04 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -27,10 +27,15 @@ public class DemoApplication {
 	String sayHello() {
 		String serviceName = System.getenv("ConnectDB");
 
+		try {
 			// call methods that might throw SQLException
 			Connection connection = DriverManager.getConnection(serviceName);
 			log.info("Connecting to the database");
 			log.info("Database connection test: " + connection.getCatalog());
+		} catch (SQLException e) {
+			// do something appropriate with the exception, *at least*:
+			e.printStackTrace();
+		}
 		return "hello";
 	}
 }

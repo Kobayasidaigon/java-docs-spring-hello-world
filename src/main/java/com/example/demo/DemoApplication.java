6d692b35caab99f06dbd4aa3da020e0b59c800cb
Kobayasidaigon <49702049+Kobayasidaigon@users.noreply.github.com>
2023-05-13 00:05:42 +0900

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,6 +11,13 @@ import java.util.logging.Logger;
 @SpringBootApplication
 @RestController
 public class DemoApplication {
+	
+    private static final Logger log;
+
+    static {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "[%4$-7s] %5$s %n");
+        log =Logger.getLogger(DemoApplication.class.getName());
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
